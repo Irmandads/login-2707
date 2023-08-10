8c1d98adba2fd665e75d1708fb010b7abcf8c6fb
bbluwbrryes <idwista@gmail.com>
2023-08-10 14:19:34 +0700

    login

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - 27 July 2023</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <br>
+    <br>
+    <center>
+        <h2>FORM LOGIN</h2>
+    </center>
+    <br>
+    <div class="login">
+        <br>
+        <form action="login.php" method="POST" onSubmit="return validasi()">
+            <div>
+                <label for="">Username : </label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="">Password : </label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <input type="submit" value="login" class="tombol">
+            </div>
+        </form>
+    </div>
+</body>
+<script type="text/javascript">
+    function validasi() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (username != "" && password != "") {
+            return true
+        } else {
+            alert('Username dan Password harus diisi');
+            return false;
+        }
+    }
+</script>
+
+</html>
